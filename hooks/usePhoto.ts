7c37560c4e3ddaf1adafe496f0bc504884ba6e72
CristinaJung <cristijung@gmail.com>
2026-05-05 16:05:55 -0300

    integração com camera e sincronização finalizada

--- hooks/usePhoto.ts
+++ hooks/usePhoto.ts
@@ -1,5 +1,6 @@
 import * as ImagePicker from "expo-image-picker";
 import { Alert } from "react-native";
+
 export const usePhoto = () => {
   const takePhoto = async () => {
     // permissão para usar a câmera
@@ -11,12 +12,14 @@ export const usePhoto = () => {
       );
       return null;
     }
+
     // abre a câmera
     const result = await ImagePicker.launchCameraAsync({
       allowsEditing: true, // permite cortar a foto
       aspect: [4, 3],
       quality: 0.7, // reduz o tamanho para não pesar no upload
     });
+    
     if (!result.canceled) {
       return result.assets[0].uri; // retorna o caminho da foto no celular
     }
